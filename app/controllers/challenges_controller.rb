e9704cf659bded09fff31e8ca784ffdfacb73915
Antoine Girard <antoine.girard@eisti.fr>
2017-02-12 19:38:11 +0100

    Edit challenge

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -3,7 +3,10 @@ class ChallengesController < ApplicationController
   authorize_resource
 
   def index
-    @challenges = Challenge.all
+    @active_challenges = Challenge.where('status' => :active)
+    @wip_challenges = Challenge.where('status' => :wip)
+    @completed_challenges = Challenge.where('status' => :completed)
+    @archived_challenges = Challenge.where('status' => :archived)
   end
 
   def new
@@ -11,7 +14,7 @@ class ChallengesController < ApplicationController
   end
 
   def create
-    @challenge = Challenge.new(challenge_params)
+    @challenge = Challenge.new(public_challenge_params)
 
     if verify_recaptcha(model: @challenge) && @challenge.save
       redirect_to challenges_path
@@ -20,9 +23,26 @@ class ChallengesController < ApplicationController
     end
   end
 
+  def edit
+    @challenge = Challenge.find(params[:id])
+  end
+
+  def update
+    @challenge = Challenge.find(params[:id])
+    if @challenge.update(challenge_params)
+      redirect_to challenges_path
+    else
+      render 'edit'
+    end
+  end
+
   private
 
-  def challenge_params
+  def public_challenge_params
     params.require(:challenge).permit(:label, :author)
+  end
+
+  def challenge_params
+    params.require(:challenge).permit(:label, :author, :status, :video_link)
   end
 end
