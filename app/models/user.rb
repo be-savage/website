83d1dcc85eda7d61b4a579f96aefc10795abc9a2
Antoine Girard <antoine.girard@eisti.fr>
2017-02-18 20:46:30 +0100

    Add users export

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,4 +30,14 @@ class User < ApplicationRecord
       where(conditions.to_h).first
     end
   end
+
+  # Export CSV
+  def self.to_csv
+    CSV.generate do |csv|
+      csv << column_names
+      all.each do |user|
+        csv << user.attributes.values_at(*column_names)
+      end
+    end
+  end
 end
