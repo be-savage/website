559fab044d9724c0b2f7b3360ff815d313d16cf1
Antoine Girard <antoine.girard@eisti.fr>
2017-03-05 00:57:20 +0100

    Add mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  ActionMailer::Base.smtp_settings = {
+      :user_name => 'no-reply',
+      :password => ENV['MAIL_PASSWORD'],
+      :domain => 'be-savage.fr',
+      :address => 'smtp.be-savage.fr',
+      :port => 587,
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
 end
