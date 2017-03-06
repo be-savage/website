8f0c506d9eac59e4c81848623377e041a814d477
Antoine Girard <antoine.girard@eisti.fr>
2017-03-06 12:48:09 +0100

    Add allos dates

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,6 @@ module Allo
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :fr
+    config.time_zone = 'Europe/Paris'
   end
 end
