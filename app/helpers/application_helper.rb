63ac9893bced0a776631d121df681380f49633e8
Antoine Girard <antoine.girard@eisti.fr>
2017-03-07 20:33:39 +0100

    Add max lenght order

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,9 @@ module ApplicationHelper
   end
 
   def allos_dates
-    if (Time.now > Time.new(2017,03,8,16,00) and Time.now < Time.new(2017,03,8,0,0)) or (Time.now > Time.new(2017,03,13,5,0) and Time.now < Time.new(2017,03,13,18,0))
+    if Rails.env.development?
+      true
+    elsif (Time.now > Time.new(2017,03,8,16,00) and Time.now < Time.new(2017,03,8,0,0)) or (Time.now > Time.new(2017,03,13,5,0) and Time.now < Time.new(2017,03,13,18,0))
       true
     else
       false
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,9 @@
 class Order < ApplicationRecord
   validates :place, presence: true
-  validates :content, presence: true
-  validates :author, presence: true
+  validates :place_details, length: { maximum: 150 }
+  validates :content, presence: true, length: { minimum: 5, maximum: 500 }
+  validates :author, presence: true, length: { minimum: 2, maximum: 150 }
+  validates :recontact_info, length: { maximum: 150 }
 
   enum status: [ :active, :archived, :completed ]
 
